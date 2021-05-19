dd1ee34685d7c9beda00f8c56176f61cdf82097e
cowens325 <corinneowens325@gmail.com>
2021-05-19 18:02:02 -0400

    added a loop to the days in JS

--- src/index.js
+++ src/index.js
@@ -14,6 +14,36 @@ function formatDate(timestamp) {
     return `${day} ${hours}: ${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    daysforEach(function(day) {
+         forecastHTML = forecastHTML + `
+         <div class="col-2">
+                            <div class="weather-forcast-date">${day}</div>
+                           
+                            <img src="http://openweathermap.org/img/wn/01d@2x.png" alt=""
+                            width="42"
+                            />
+                            <div class="weather-forecast-temperature">
+                           <span class="weather-forecast-temperature-max" > 18°</span>
+                        <span class="weather-forecast-temperature-min">
+                         12° </span>
+
+                         </div>
+                    </div>
+             
+                `;
+
+    })
+   
+   
+                       
+        forecastHTML = forecastHTML + `</div>`;
+        forecastElement.innerHTML = forecastHTML;
+}
 
 
 
@@ -27,6 +57,8 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
+
+    displayForecast();
 
     fahrenheitTemperature = response.data.main.temp;
 
